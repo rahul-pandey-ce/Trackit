946ce24662ff5e409ea60c88bc42b198d9f209e6
Rahul Pandey <rahulbaba3090@gmail.com>
2026-05-17 15:21:37 +0530

    feat: Update main.dart MaterialApp title to ensure tab title persistence

--- lib/main.dart
+++ lib/main.dart
@@ -70,7 +70,7 @@ class TrackItApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'TRACKIT',
+      title: 'TRACKIT: Study Tracker',
       theme: ThemeData(
         fontFamily: 'Inter',
         useMaterial3: true,
